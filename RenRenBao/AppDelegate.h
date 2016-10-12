b2744f7aed32c401ba8a921e3f35582d6879ad50
张强 <xiaojuhua@zhangqingdeimac.lan>
2016-10-12 17:18:17 +0800

    Initial Commit

---
+++ RenRenBao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RenRenBao
+//
+//  Created by 张强 on 16/10/12.
+//  Copyright © 2016年 chuangen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
